53a59899e9668892556b109ad664429329a1b122
Vandy Sodanheang <56287711+VandyTheCoder@users.noreply.github.com>
2023-09-29 11:32:18 +0700

    Update init.sql

--- oracle/init-script/init.sql
+++ oracle/init-script/init.sql
@@ -1,5 +1,5 @@
 -- Create Default Schema Hello World
-CREATE USER 'hello-world' IDENTIFIED BY 'hello-world-password';
-GRANT CONNECT, RESOURCE TO 'hello-world';
-ALTER USER 'hello-world' QUOTA UNLIMITED ON USERS;
+CREATE USER hello-world IDENTIFIED BY hello-world-password;
+GRANT CONNECT, RESOURCE TO hello-world;
+ALTER USER hello-world QUOTA UNLIMITED ON USERS;
 exit;
